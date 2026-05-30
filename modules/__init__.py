5dba41faf6870b26116702682eae78ce4ea99bd6
Elonian <varun.sm@outlook.com>
2026-05-30 05:34:01 +0200

    Add modules package exports

---
+++ modules/__init__.py
@@ -0,0 +1,27 @@
+"""Canonical model modules for PoseBeat-MusicGen."""
+
+from .audio_generator import (
+    AudioPipelineComponents,
+    MotionConditionedUNet,
+    create_conditioned_unet,
+    create_noise_scheduler,
+    freeze_module,
+    load_audio_generator_components,
+    load_audio_pipeline_components,
+    save_audio_pipeline,
+)
+from .motion_adapter import MotionAdapter, MotionAdapterConfig, MotionConditioning
+
+__all__ = [
+    "AudioPipelineComponents",
+    "MotionAdapter",
+    "MotionAdapterConfig",
+    "MotionConditionedUNet",
+    "MotionConditioning",
+    "create_conditioned_unet",
+    "create_noise_scheduler",
+    "freeze_module",
+    "load_audio_generator_components",
+    "load_audio_pipeline_components",
+    "save_audio_pipeline",
+]
